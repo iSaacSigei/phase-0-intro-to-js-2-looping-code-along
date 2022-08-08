169400e941b6a5a90a7beec316089106ead4679f
iSaacSigei <sigei.isack@student.moringaschool.com>
2022-08-08 16:32:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+//for loop function
+function writeCards(names, event) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+console.log(writeCards(['ada', 'Brendan', 'Ali'], 'birthday'));
+
+
+//countdown
+
+function countDown() {
+  let countDown = 10;
+  while (countDown >= 0) {
+    console.log(countDown--);
+    
+  }
+  
+}
